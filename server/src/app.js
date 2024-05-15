c597b5c93b31c8907dd9d6f2fae7b7f8f14a7bc2
Carlos-VG <cvivasg@unicauca.edu.co>
2024-05-14 23:17:37 -0500

    Probando cositas

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,20 @@
+/**
+ * @file app.js
+ * @description Archivo principal de la aplicación
+ */
+
 const express = require('express');
 const config = require('./config');
 
+const docentes = require('./modulos/docentes/rutas');
+
 const app = express();
 
+/**
+ * @brief Configuración de la aplicación
+ */
 app.set('port', config.app.port);
+
+app.use('/api/docentes', docentes);
 
 module.exports = app;
